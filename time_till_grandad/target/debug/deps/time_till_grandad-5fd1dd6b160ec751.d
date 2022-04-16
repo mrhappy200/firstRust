9ff0962b757e5414f81185cbe00b836f8a029eab
mrhappy200 <193763@gsf.nl>
2022-04-16 19:47:07 +0200

    thing

---
+++ time_till_grandad/target/debug/deps/time_till_grandad-5fd1dd6b160ec751.d
@@ -0,0 +1,5 @@
+/home/mrhappy200/projects/firstRust/time_till_grandad/target/debug/deps/time_till_grandad-5fd1dd6b160ec751.rmeta: src/main.rs
+
+/home/mrhappy200/projects/firstRust/time_till_grandad/target/debug/deps/time_till_grandad-5fd1dd6b160ec751.d: src/main.rs
+
+src/main.rs:
